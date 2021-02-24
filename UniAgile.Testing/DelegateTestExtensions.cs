1c252b108bd124fd79f233714b573a1016717a01
Roni Jokinen <roni3jokinen@gmail.com>
2021-02-24 21:29:57 +0200

    added keywords for funcs and action with param

--- UniAgile.Testing/DelegateTestExtensions.cs
+++ UniAgile.Testing/DelegateTestExtensions.cs
@@ -32,6 +32,62 @@ namespace UniAgile.Testing
         {
             foreach (var delegateMock in delegateMocks) delegateMock.is_not_called();
         }
+
+        private static void called<T>(this Mock<Action<T>> delegateMock)
+        {
+            delegateMock.Verify(listener => listener.Invoke(It.IsAny<T>()), Times.Exactly(1), "Delegate was not called. It was expected to be called");
+        }
+
+        public static void are_called_once<T>(this Mock<Action<T>>[] delegateMock)
+        {
+            foreach (var mock in delegateMock)
+            {
+                called(mock);
+            }
+        }
+        
+        public static void is_called_once<T>(this Mock<Action<T>> delegateMock)
+        {
+            called(delegateMock);
+        }
+
+        public static void is_not_called<T>(this Mock<Action<T>> delegateMock)
+        {
+            delegateMock.Verify(listener => listener.Invoke(It.IsAny<T>()), Times.Exactly(0), "Delegate was called. It was expected not to be called");
+        }
+
+        public static void are_not_called<T>(this Mock<Action<T>>[] delegateMocks)
+        {
+            foreach (var delegateMock in delegateMocks) delegateMock.is_not_called();
+        }
+
+        private static void called<T>(this Mock<Func<T>> delegateMock)
+        {
+            delegateMock.Verify(listener => listener.Invoke(), Times.Exactly(1), "Delegate was not called. It was expected to be called");
+        }
+
+        public static void are_called_once<T>(this Mock<Func<T>>[] delegateMock)
+        {
+            foreach (var mock in delegateMock)
+            {
+                called(mock);
+            }
+        }
+        
+        public static void is_called_once<T>(this Mock<Func<T>> delegateMock)
+        {
+            called(delegateMock);
+        }
+
+        public static void is_not_called<T>(this Mock<Func<T>> delegateMock)
+        {
+            delegateMock.Verify(listener => listener.Invoke(), Times.Exactly(0), "Delegate was called. It was expected not to be called");
+        }
+
+        public static void are_not_called<T>(this Mock<Func<T>>[] delegateMocks)
+        {
+            foreach (var delegateMock in delegateMocks) delegateMock.is_not_called();
+        }
         
     }
 }
