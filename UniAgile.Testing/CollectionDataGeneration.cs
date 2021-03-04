b4bca0a65f0664cf7e9f391e978372b6c80774d1
Roni Jokinen <roni3jokinen@gmail.com>
2021-03-04 03:15:03 +0200

    feature flag and listenable data generation

---
+++ UniAgile.Testing/CollectionDataGeneration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+// ReSharper disable PossibleMistakenCallToGetType.2
+
+namespace UniAgile.Testing
+{
+    public static class CollectionDataGeneration
+    {
+        public static Dictionary<string, TEnum> FeatureFlag<TEnum>(TEnum value)
+            where TEnum : Enum
+        {
+            return new Dictionary<string, TEnum>()
+            {
+                {typeof(TEnum).GetType().ToString(), value}
+            };
+        }
+
+        public static Dictionary<string, INotifyCollectionChanged> EmptyListenables()
+        {
+            return new Dictionary<string, INotifyCollectionChanged>();
+        }
+        
+        public static Dictionary<string, INotifyCollectionChanged> Listenables(string key, INotifyCollectionChanged collectionChanged)
+        {
+            return new Dictionary<string, INotifyCollectionChanged>()
+            {
+                {
+                    key, collectionChanged
+                }
+            };
+        }
+    }
+}
